21f70ba7155f01b2be94b3420ad24d5188378630
dannyrb <danny.ri.brown@gmail.com>
2017-11-29 01:35:17 -0500

    Adding a default route

---
+++ src/router/DefaultRoute.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-container">
+    <h1>{{msg}}</h1>
+
+    <cornerstone-canvas></cornerstone-canvas>
+
+  </div>
+</template>
+
+<script>
+import CornerstoneCanvas from './../components/CornerstoneCanvas'
+
+export default {
+  name: 'DefaultRoute',
+  components: {
+    CornerstoneCanvas
+  },
+  data () {
+    return {
+      msg: 'Hello Cornerstone (:'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
